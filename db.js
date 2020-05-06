37cb1887623e1d9727e820729317eed29f2da408
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-05-06 17:57:04 -0400

    Turn off CORS checking

--- db.js
+++ db.js
@@ -29,7 +29,10 @@ const Hapi = require("@hapi/hapi");
 const init = async () => {
 	const server = Hapi.server({
 		host: "localhost",
-		port: 3000
+		port: 3000,
+		routes: {
+			cors: true
+		}
 	})
 
 	// Output endpoints at startup.
