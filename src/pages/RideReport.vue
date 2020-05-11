17f64e154c13bea036eafbc4e170dff331d64b51
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-05-11 11:24:30 -0400

    Parse ride dates

--- src/pages/RideReport.vue
+++ src/pages/RideReport.vue
@@ -1,51 +1,53 @@
 <template>
-    <v-container>
-        <h2>
-            Rides
-        </h2>
-        <instructions details="View upcoming rides here."></instructions>
-        <v-data-table
-            :headers="headers"
-            :items="rides"
-            :items-per-page="5"
-            class="elevation-1"
-        ></v-data-table>
-    </v-container>
+  <v-container>
+    <h2>
+      Rides
+    </h2>
+    <instructions details="View upcoming rides here."></instructions>
+    <v-data-table
+      :headers="headers"
+      :items="rides"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </v-container>
 </template>
 
 <script>
-    import Instructions from "../components/Instructions";
-    export default {
-        components: {Instructions},
-        data () {
-            return {
-                headers: [
-                    {
-                        text: 'Date',
-                        align: 'start',
-                        sortable: false,
-                        value: 'date',
-                    },
-                    { text: 'Time', value: 'time' },
-                    { text: 'To', value: 'tolocationid' },
-                    { text: 'From', value: 'fromlocationid' },
-                    { text: 'Fee', value: 'fee' },
-                    { text: 'Distance', value: 'distance' },
-                    { text: 'Drivers', value: 'drivers' },
-                    { text: 'Passengers', value: 'passengers' },
-                ],
-                rides: [],
-            }
+import Instructions from "../components/Instructions";
+export default {
+  name: "RideReport",
+  components: { Instructions },
+  data() {
+    return {
+      headers: [
+        {
+          text: "Date",
+          align: "start",
+          sortable: false,
+          value: "date",
         },
-        mounted: async function() {
-            console.log("about to GET");
-            let response;
-            response = await this.$axios.get("/rides");
-            console.log(`GET  happened ${response}`);
-            this.rides = response.data.map( (obj) => {
-                console.log(obj.date.parse());
-                return obj;
-            });
-        },
-    }
+        { text: "Time", value: "time" },
+        { text: "To", value: "tolocationid" },
+        { text: "From", value: "fromlocationid" },
+        { text: "Fee", value: "fee" },
+        { text: "Distance", value: "distance" },
+        { text: "Drivers", value: "drivers" },
+        { text: "Passengers", value: "passengers" },
+      ],
+      rides: [],
+    };
+  },
+  mounted: async function() {
+    console.log("about to GET");
+    let response;
+    response = await this.$axios.get("/rides");
+    console.log("GET  happened", response);
+    this.rides = response.data.map((obj) => {
+      // console.log(obj.date.parse());
+      obj.date = new Date(obj.date);
+      return obj;
+    });
+  },
+};
 </script>
