19347aaa1a5c87571bd0ecf09156173422c98ffc
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-05-14 14:25:27 -0400

    Add template click handler

--- src/pages/PassengerRides.vue
+++ src/pages/PassengerRides.vue
@@ -1,43 +1,38 @@
 <template>
-    <div class="text-center">
-<!--        <v-menu offset-y>-->
-<!--            <template v-slot:activator="scope">-->
-<!--                <v-btn-->
-<!--                        color="primary"-->
-<!--                        dark-->
-<!--                        v-on="scope.on"-->
-<!--                >-->
-<!--                    Dropdown-->
-<!--                </v-btn>-->
-<!--            </template>-->
-<!--            <v-list>-->
-<!--                <v-list-item-->
-<!--                        v-for="(item, index) in items"-->
-<!--                        :key="index"-->
-<!--                        @click=""-->
-<!--                >-->
-<!--                    <v-list-item-title>{{ item.title }}</v-list-item-title>-->
-<!--                </v-list-item>-->
-<!--            </v-list>-->
-<!--        </v-menu>-->
-    </div>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="scope">
+        <v-btn color="primary" dark v-on="scope.on">
+          Dropdown
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="(item, index) in items" :key="index" @click="itemClicked(item)">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 
 <script>
-    export default {
-        data: () => ({
-            items: [
-                { title: 'Click Me' },
-                { title: 'Click Me' },
-                { title: 'Click Me' },
-                { title: 'Click Me 2' },
-            ],
-        }),
-    }
+export default {
+  data: () => ({
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" },
+    ],
+  }),
+
+  methods: {
+      itemClicked(item) {
+          console.log("CLICK on item", item.title);
+      }
+  }
+};
 </script>
-
-
-
 
 <!--<template>-->
 <!--    <v-container>-->
